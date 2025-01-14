28ae9d6a2cb28772579460f0280b8d15cb21e102
ishwarkavhale <52922739+ishwarkavhale@users.noreply.github.com>
2025-01-14 09:56:54 +0530

    Updated input variables

--- EKS_Terraform/variables.tf
+++ EKS_Terraform/variables.tf
@@ -1,5 +1,23 @@
+variable "region" {
+  description = "The AWS region to deploy resources in"
+  type        = string
+  default     = "us-east-1"  # Optional default value
+}
+
+variable "instance_type" {
+  description = "Type of the EC2 instance"
+  type        = string
+}
+
+variable "instance_count" {
+  description = "Number of instances to create"
+  type        = number
+  default     = 1
+}
+
 variable "ssh_key_name" {
   description = "The name of the SSH key pair to use for instances"
   type        = string
   default     = "DevOps"
 }
+
